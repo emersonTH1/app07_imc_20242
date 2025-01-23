7f707f0fb063347d966fbc1af4dceb742846e3f0
emersonTH1 <hanccotomayllaemerson@gmail.com>
2025-01-23 10:00:42 -0500

    se colmino

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,13 +7,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFF003049),
+        backgroundColor: Color.fromARGB(255, 39, 134, 185),
         title: Text("IMC App - 2025"),
         centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               "Bienvenido, selecciona tu peso y altura",
@@ -22,9 +23,7 @@ class HomePage extends StatelessWidget {
                 color: Color(0xFF003049),
               ),
             ),
-            SizedBox(
-              height: 10.0,
-            ),
+            SizedBox(height: 10.0),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.baseline,
@@ -35,12 +34,10 @@ class HomePage extends StatelessWidget {
                   style: TextStyle(
                     fontSize: 30.0,
                     fontWeight: FontWeight.bold,
-                    color: Color(0xFF003049),
+                    color: Color.fromARGB(255, 41, 172, 243),
                   ),
                 ),
-                SizedBox(
-                  width: 2.0,
-                ),
+                SizedBox(width: 2.0),
                 Text(
                   "Kg",
                   style: TextStyle(
@@ -56,14 +53,8 @@ class HomePage extends StatelessWidget {
               min: 20,
               max: 200,
               onChanged: (double value) {},
-              // activeColor: Colors.amber,
-              // inactiveColor: Colors.blue,
-              // overlayColor: Color(Colors.amber) ,
-              // thumbColor: Colors.red,
             ),
-            SizedBox(
-              height: 10.0,
-            ),
+            SizedBox(height: 10.0),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.baseline,
@@ -77,9 +68,7 @@ class HomePage extends StatelessWidget {
                     color: Color(0xFF003049),
                   ),
                 ),
-                SizedBox(
-                  width: 2.0,
-                ),
+                SizedBox(width: 2.0),
                 Text(
                   "cm",
                   style: TextStyle(
@@ -96,9 +85,7 @@ class HomePage extends StatelessWidget {
               max: 200,
               onChanged: (double value) {},
             ),
-            SizedBox(
-              height: 10.0,
-            ),
+            SizedBox(height: 10.0),
             SizedBox(
               width: double.infinity,
               height: 50.0,
@@ -115,6 +102,61 @@ class HomePage extends StatelessWidget {
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Color(0xFF003049),
                 ),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Divider(
+              height: 10.0,
+              color: Color(0xFF003049),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              "Resultado:",
+              style: TextStyle(
+                fontSize: 15.0,
+                color: Color(0xFF003049),
+              ),
+            ),
+            Center(
+              child: Image.asset(
+                "assets/images/image4.png",
+                height: 200.0,
+                width: 200.0,
+                fit: BoxFit.contain,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Center(
+              child: Column(
+                children: [
+                  Text(
+                    "25.4",
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red,
+                    ),
+                  ),
+                  Text(
+                    "Sobrepeso",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF003049),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Toma agua simple, evita el consumo de refrescos, jugos o cualquier bebida que contenga azúcar. Realiza actividad física.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
