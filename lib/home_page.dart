a2ae1a0e27d811549e9bd140fc617c861ca8d221
emersonTH1 <hanccotomayllaemerson@gmail.com>
2025-01-23 07:35:40 -0500

    avazado

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,6 +11,115 @@ class HomePage extends StatelessWidget {
         title: Text("IMC App - 2025"),
         centerTitle: true,
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            Text(
+              "Bienvenido, selecciona tu peso y altura",
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Color(0xFF003049),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.baseline,
+              textBaseline: TextBaseline.alphabetic,
+              children: [
+                Text(
+                  "70",
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF003049),
+                  ),
+                ),
+                SizedBox(
+                  width: 2.0,
+                ),
+                Text(
+                  "Kg",
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w300,
+                    color: Color(0xFF003049),
+                  ),
+                ),
+              ],
+            ),
+            Slider(
+              value: 70,
+              min: 20,
+              max: 200,
+              onChanged: (double value) {},
+              // activeColor: Colors.amber,
+              // inactiveColor: Colors.blue,
+              // overlayColor: Color(Colors.amber) ,
+              // thumbColor: Colors.red,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.baseline,
+              textBaseline: TextBaseline.alphabetic,
+              children: [
+                Text(
+                  "175",
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF003049),
+                  ),
+                ),
+                SizedBox(
+                  width: 2.0,
+                ),
+                Text(
+                  "cm",
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w300,
+                    color: Color(0xFF003049),
+                  ),
+                ),
+              ],
+            ),
+            Slider(
+              value: 70,
+              min: 20,
+              max: 200,
+              onChanged: (double value) {},
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 50.0,
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                label: Text(
+                  "Calcular",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.0,
+                  ),
+                ),
+                icon: Icon(Icons.play_arrow_rounded),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF003049),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app07_imc_20242/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -10,7 +11,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'IMC App',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: false),
+      theme: ThemeData(
+        useMaterial3: false,
+        sliderTheme: SliderThemeData(
+          activeTrackColor: Color(0xFFc1121f),
+          inactiveTrackColor: Color(0xFFfdf0d5),
+          thumbColor: Color(0xFF669bbc),
+          overlayColor: Color(0xFF780000).withOpacity(0.3),
+        ),
+      ),
       home: HomePage(),
     );
   }
